526bc52be605f76f5370d073ed522dc6e09f33a7
Adam Hoddinott <adam@messymind.net>
2013-03-05 11:12:17 +1100

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup( name='pages',
+setup( name='posts',
     version = '0.0.1',
     description = 'Posts, based on entropy.',
     author = 'Daryl Antony',
