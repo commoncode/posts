7bfc45821df1edb6193cb7f65d300750e196f5c5
Adam Hoddinott <adam@messymind.net>
2013-03-25 14:22:33 +1100

    Changing default listview url

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('',
-     url(r'^posts/$', 'posts.views.all_posts', name='posts_all_posts'),
+     url(r'^$', 'posts.views.all_posts', name='posts_all_posts'),
      url(r'^(?P<slug>[-\w]+)/$', 'posts.views.detail', name='posts_detail_post'),
 )
